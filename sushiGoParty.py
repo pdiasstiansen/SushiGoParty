ba5cae6f5d37ac9d80e967946297bf7059eaf534
pdiasstiansen <37642548+pdiasstiansen@users.noreply.github.com>
2018-08-23 14:48:46 -0700

    8/23 work

--- sushiGoParty.py
+++ sushiGoParty.py
@@ -33,16 +33,11 @@ cardList = [['Nigiri', 0, 12],
             ['Green Tea Ice Cream', 21, 15],
             ['Fruit', 22, 15]]
 
-cards = []
-
-def makeCards():
-    for card in cardList:
-        for count in range(card[2]):
-            cards.append(Cards(card))
-
 class Player:
 
-    count = 1
+    playerCount = 0
+    count = 1 # end of game need to reset this to empty list
+    playerGroup = [] # end of game need to reset this to empty list
 
     def __init__(self):
         self.hand = []
@@ -57,21 +52,117 @@ class Player:
     def chooseCard(self):
         choice = listMaker(self.hand)
 
-    def dealHand(self, size):
-        random.shuffle(deck)
-        for x in range(size):
-            self.hand.append(deck.pop(0))
+    def dealHand(self):
+        temp_handSize = self.handAmount()##move this elsewhere, it only needs to be run after player ammount is chosen
+        print(temp_handSize)
+        print(len(menue.menue))
+        for x in range(temp_handSize):
+            self.hand.append(menue.menue.pop(0))
+
+    def handAmount(self):
+        if Player.playerCount == 8:
+            return 7
+        elif Player.playerCount > 6:
+            return 8
+        elif Player.playerCount > 4:
+            return 9
+        else:
+            return 10
 
 class Menue:
 
     def __init__(self):
         self.menue = []
-        self.myFirstMeal = []
+        self.dessert = []
+        self.myFirstMealList = ['Nigiri', 'Maki Roll', 'Tempura', 'Sashimi', 'Miso Soup', 'Wasabi', 'Tea', 'Green Tea Ice Cream']
 
+    def pickMeal(self):
+        pass
+        
     def myFirstMeal(self):
-        for card in cardList:
-            if card.name in self.myFirstMeal:
+        for card in game.cards:
+            if card.name in self.myFirstMealList and card.foodType < 20:
                 self.menue.append(card)
+                print(card)
+        for card in game.cards:
+            if card.name in self.myFirstMealList and card.foodType >= 20:
+                self.dessert.append(card)
+                print('Dessert: ' , card)
+        self.dealDessert()
+
+    ##Put rest of meal codes here
+
+    def dessertAmount(self, playerCount, roundCount):
+        if playerCount <= 5:
+            if roundCount < 3:
+                return 7 - (2*roundCount)
+            else:
+                return 2
+        elif playerCount >= 6:
+            return 9 - (2*roundCount)
+
+    def dealDessert(self):
+        amount = self.dessertAmount(Player.playerCount, game.roundCount)
+        for x in range(amount):
+            temp_cardToAdd = self.dessert.pop(0)
+            self.menue.append(temp_cardToAdd)
+        random.shuffle(self.menue)
+
+class Game:
+
+    def __init__(self):
+        self.mode = 0
+        self.roundCount = 1
+        self.cards = []
+
+    def pickMode(self):
+        gameModes = ['My First Meal', 'Sushi Go!', 'Party Sampler', 'Master Menu', 'Points Platter', 'Cutthroat Combo', 'Big Banquet', 'Dinner For Two']
+        while True:
+            print('Which meal will you choose (1-8)?')
+            modeChoice = listMaker(gameModes)
+            if modeChoice in gameModes:
+                modeChoice = int(gameModes.index(modeChoice))
+                self.mode = modeChoice
+                break
+            else:
+                print('Please select a valid game mode.')
+
+    def createDeck(self):
+        if self.mode == 0:
+            menue.myFirstMeal()
+        elif self.mode == 1:
+            pass
+        elif self.mode == 2:
+            pass
+        elif self.mode == 3:
+            pass
+        elif self.mode == 4:
+            pass
+        elif self.mode == 5:
+            pass
+        elif self.mode == 6:
+            pass
+        elif self.mode == 7:
+            pass
+
+    def playerCount(self):
+        while True:
+            playerCount = input('How many players? (2-8)')
+            if isADigit(playerCount) == True and playerCount >= '2' and playerCount <= '8':
+                Player.playerCount = int(playerCount)
+                print(Player.playerCount)
+                for x in range(Player.playerCount):
+                    Player.playerGroup.append(Player())
+                break
+            else:
+                print('Please select a valid number of players.')
+
+    def makeCards(self):
+        for card in cardList:
+            for count in range(card[2]):
+                self.cards.append(Cards(card))
+        
+                
 
 def isADigit(digit):
     if type(digit) == int:
@@ -103,118 +194,27 @@ def listMaker(listOfChoices):
             print('You must select a valid option')
         else:
             print('You must select a valid option')
-
-def playerCount():
-    while True:
-        playerCount = input('How many players? (2-8)')
-        if isADigit(playerCount) == True and playerCount >= '2' and playerCount <= '8':
-            playerCount = int(playerCount)
-            playerGroup = []
-            for x in range(playerCount):
-                playerGroup.append(Player())
-            return playerGroup
-            break
-        else:
-            print('Please select a valid number of players.')
-
-def gameMode():
-    gameModes = ['My First Meal', 'Sushi Go!', 'Party Sampler', 'Master Menu', 'Points Platter', 'Cutthroat Combo', 'Big Banquet', 'Dinner For Two']
-    while True:
-        print('Which meal will you choose (1-8)?')
-        modeChoice = listMaker(gameModes)
-        if modeChoice in gameModes:
-            modeChoice = int(gameModes.index(modeChoice))
-            return modeChoice
-            break
-        else:
-            print('Please select a valid game mode.')
-
-def createDeck(gameMode):
-    deck =[]
-    if gameMode == 0:
-        mode = ['Nigiri', 'Maki Roll', 'Tempura', 'Sashimi', 'Miso Soup', 'Wasabi', 'Tea', 'Green Tea Ice Cream']
-        for card in cards:
-            print(card)
-            if card.name in mode:
-                deck.append(card)
-        return deck
-    elif gameMode == 1:
-        pass
-    elif gameMode == 2:
-        pass
-    elif gameMode == 3:
-        pass
-    elif gameMode == 4:
-        pass
-    elif gameMode == 5:
-        pass
-    elif gameMode == 6:
-        pass
-    elif gameMode == 7:
-        pass
-
-def removeDessert():
-    global deck
-    dessertPile = []
-    while deck[-1].foodType >= 20:
-        dessertPile.append(deck[-1])
-        deck.pop(-1)
-    return dessertPile
-
-def dessertAmount(playerCount, roundCount):
-    if playerCount <= 5:
-        if roundCount < 3:
-            return 7 - (2*roundCount)
-        else:
-            return 2
-    elif playerCount >= 6:
-        return 9 - (2*roundCount)
-
-def dealDessert(playerCount, roundCount):
-    global dessertPile
-    temp_deck = []
-    amount = dessertAmount(playerCount, roundCount)
-    for x in range(amount):
-        print(x)
-        temp_deck.append(dessertPile.pop(0))
-    return temp_deck
-
-def dealHand(playerCount):
-    if playerCount == 8:
-        amount = 7
-    elif playerCount > 6:
-        amount = 8
-    elif playerCount > 4:
-        amount = 9
-    else:
-        amount = 10
-    for player in playerGroup:
-        player.dealHand(amount)
             
 
-def roundLoop(number):
+def roundLoop(number): # integrate into game class
     rotation = 0
     if number % 2 == 0:
         rotation = 1
     for player in playerGroup:
         player.chooseCard()
 
-roundCount = 1
-playerGroup = playerCount()
-playerCount = len(playerGroup)
-makeCards()
-gameMode = gameMode()
-deck = createDeck(gameMode)
-dessertPile = removeDessert()
-deck.extend(dealDessert(playerCount, roundCount))
-dealHand(playerCount)
+game = Game()
+menue = Menue()
+game.playerCount()
+game.makeCards()
+game.pickMode()
+game.createDeck()
+print('playerGroupsize: ' , len(Player.playerGroup)) 
+for player in Player.playerGroup:
+    player.dealHand()
 
-print(playerGroup)
-print(gameMode)
-print(deck)
-print(dessertPile)
 
-for player in playerGroup:
+for player in Player.playerGroup:
     print(player.hand)
 
 
